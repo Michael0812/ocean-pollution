4c29e5da7bf3705885e54d7fc8cd5b2dfa14c568
Michael0812 <michalwesolowski11@gmail.com>
2020-01-16 15:04:36 +0000

    added chat texbox function

---
+++ Desktop/flask-chat/README.md
@@ -0,0 +1,3 @@
+# Flask Chat App
+
+This is a chat application written in Flask. This purpose of this project is to take data from a URL and store the information in a list.
---
+++ Desktop/flask-chat/run.py
@@ -0,0 +1,45 @@
+import os
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
+
+app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
+
+
+def add_message(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(url_for('user', username=session['username']))
+
+    return render_template("index.html")
+
+
+@app.route("/chat/<username>", methods=["GET", "POST"])
+def user(username):
+    """Add and display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(session["username"])
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
+
+
+
+if __name__ =="__main__":
+    app.run(host=os.getenv("IP"),
+       port=int(os.getenv("PORT", "8000")),
+       debug=True)
